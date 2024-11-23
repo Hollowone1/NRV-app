bca04f20838b7fb325fdf203cee1fb58335b4d11
Antoine Stoecklin <astoeck@hotmail.fr>
2024-11-23 09:27:57 +0100

    Refactor routes to organize command, evening, spot, show, and user endpoints; remove deprecated routes

--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,42 @@
 
 use App\Http\Controllers\CommandController;
 use App\Http\Controllers\EveningController;
-use App\Http\Controllers\PlaceController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\SpotController;
+use App\Http\Controllers\ShowController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 
-Route::prefix('evenings')->group(function(){
-    Route::get('{id}, /' , [EveningController::class, 'getEveningById'])->name('getEveningById');
-    Route::get('dates', [EveningController::class, 'getAllDatesEvening'])->name('getAllDatesEvening');
-    Route::get('spots', [EveningController::class, 'getAllSpotsEvening'])->name('getAllSpotsEvening');
-    Route::get('thematics', [EveningController::class, 'getAllThematicEvening'])->name('getAllThematicEvening');
+Route::prefix('commands')->group(function () {
+    Route::get('/{id}', [CommandController::class, 'getCommand'])->name('commands.show'); 
+    Route::get('/user/{mail_client}', [CommandController::class, 'getCommandsByUser'])->name('commands.user'); 
+    Route::patch('/{id}/validate', [CommandController::class, 'patchValiderCommande'])->name('commands.validate');
+    Route::post('/', [CommandController::class, 'postCreerCommande'])->name('commands.create');
 });
 
 
-
-Route::prefix('commands')->group(function(){
-    Route::get('{id}', [CommandController::class, 'getCommandById'])->name('getCommandById');
-    Route::post('/', [CommandController::class, 'createCommand'])->name('createCommand');
-    Route::patch('{id}', [CommandController::class, 'validateCommand'])->name('validateCommand');
-    Route::get('user/{mail}', [CommandController::class, 'getCommandByUser'])->name('getCommandByUser');
+Route::prefix('evenings')->group(function () {
+    Route::get('/', [EveningController::class, 'getAllDatesEvening'])->name('evenings.index'); 
+    Route::get('/{id}', [EveningController::class, 'getEveningById'])->name('evenings.show');
+    Route::get('/thematics', [EveningController::class, 'getAllThematics'])->name('evenings.thematics'); 
+    Route::get('/{id}/places', [EveningController::class, 'getNbPlace'])->name('evenings.places'); 
 });
-    
 
-Route::prefix('auth')->group(function(){
-    Route::get('/', [ProfileController::class, 'getUser'])->name('getUser');
-    Route::post('signin', [ProfileController::class, 'signin'])->name('signin');
-    Route::post('signup', [ProfileController::class, 'signup'])->name('signup');
-    Route::post('refresh', [ProfileController::class, 'refresh'])->name('refresh');
+
+Route::prefix('spots')->group(function () {
+    Route::get('/', [SpotController::class, 'getAllSpots'])->name('spots.index'); 
+    Route::get('/names', [SpotController::class, 'getAllNameSpots'])->name('spots.names'); 
 });
+
+
+Route::prefix('shows')->group(function () {
+    Route::get('/', [ShowController::class, 'getShows'])->name('shows.index'); 
+});
+
+
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'getUser'])->name('users.show'); 
+});
+
 
 require __DIR__.'/auth.php';
